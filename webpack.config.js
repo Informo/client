9c48fb3dad6f0c8d49d03802b549ffea5f67c335
Gordon F <magicdoge@sofa.wow>
2017-12-17 15:09:42 +0100

    Added SCSS generation

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,49 @@
+const isDevServer = process.argv[1].indexOf('webpack-dev-server') !== -1;
 
-var plugins = [];
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const extractSass = new ExtractTextPlugin({
+    filename: (isDevServer ? 'dist/' : '') + 'style.css',
+    // disable: process.env.NODE_ENV === "development"
+});
 
 module.exports = {
-    devtool: 'source-map',
-    resolve: {
-        alias: {}
-    },
-    output: {
-        //publicPath is required and needs to be a url
-        publicPath: 'http://localhost:8080/',
-    },
-    plugins: plugins,
-    module: {
-        loaders: [
-            {test: /\.js$/,
-                exclude: /node_modules/,
-                loader: 'babel',
-                query: {
-                    presets: ['es2015']
-                }
-            }
-        ]
-    }
+	entry: ['./app/index.js', './scss/main.scss'],
+	devtool: 'source-map',
+	resolve: {
+		alias: {}
+	},
+	output: {
+		//publicPath is required and needs to be a url
+		publicPath: 'http://localhost:8080/',
+		path: __dirname + '/dist'
+	},
+	plugins: [
+		extractSass,
+	],
+	module: {
+		loaders: [
+			{test: /\.js$/,
+				exclude: /node_modules/,
+				loader: 'babel-loader',
+				query: {
+					presets: ['env']
+				}
+			}
+		],
+
+		rules: [
+			{
+	            test: /\.scss$/,
+				use: extractSass.extract({
+	                use: [{
+	                    loader: "css-loader"
+	                }, {
+	                    loader: "sass-loader"
+	                }],
+	                // use style-loader in development
+	                fallback: "style-loader"
+	            })
+	        }
+		]
+	}
 };
