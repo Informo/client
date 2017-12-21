195743aacb5bcb211ad1bd5d1737dfe2f50eefed
Vabd <vabd@anon.acme>
2017-12-21 13:07:03 +0100

    Add hardcoded list of entrypoints

---
+++ app/entrypoint/default.js
@@ -0,0 +1,30 @@
+export default {
+    "entrypoints": [
+        "#zHtvygqcek:matrix.qarte.de",
+        "#LEQIyPXXpB:perthchat.org",
+        "#informo:hackerspace.be",
+        "#GhKTQBMJaG:hackerspace.be",
+        "#FCoBUIDTsO:zombiehost.net",
+        "#KylZmxAEfF:matrix.allmende.io",
+        "#sHyzFImjog:jcg.re",
+        "#informo:matrix.org",
+        "#FRyIPQlrVO:charity.az",
+        "#juzUkFbcNr:hispagatos.org",
+        "#aMTaQiSZVr:matrixim.cc",
+        "#informo:tchncs.de",
+        "#soZPUlLugw:tchncs.de"
+    ],
+    "nodes": [
+        "matrix.allmende.io.",
+        "jcg.re.",
+        "matrix.hispagatos.org.",
+        "matrixim.cc",
+        "matrix.tchncs.de.",
+        "perthchat.org",
+        "matrix.hackerspace.be.",
+        "matrix.zombiehost.net.",
+        "matrix.qarte.de",
+        "matrix.org.",
+        "charity.az"
+    ]
+}
