f2a39662f0f1b07f3de910eb075f08ad76a51871
Gordon F <gordonf@informo.network>
2018-04-20 23:03:58 +0200

    Added room alias to connection form

--- app/pages/connect.js
+++ app/pages/connect.js
@@ -9,29 +9,34 @@ import Materialize from "materialize-css";
 import * as sidebarPage from "./sidebar";
 
 
+const homeservers = [
+	{
+		url: "https://matrix.org",
+		room: "#informo-test:matrix.org",
+		location: "UK",
+		owner: "Matrix team",
+		info: "Official Matrix server",
+	}, {
+		url: "https://doge.wow",
+		room: "#informo:doge.wow",
+		location: "MagicLand",
+		owner: "A dog",
+		info: "Wow",
+	}
+];
+
 export function init(){
 
 	$("#form-connect-homeserver-error").text("");
 
-	let homeservers = [
-		{
-			url: "https://matrix.org",
-			location: "UK",
-			owner: "Matrix team",
-			info: "Official Matrix server",
-		}, {
-			url: "https://doge.wow",
-			location: "MagicLand",
-			owner: "A dog",
-			info: "Wow",
-		}
-	];
+
 	let homeserverList = $("#connect-homeserver-list");
 	for(let homeserver of homeservers){
 
 		let tr = $(`
 			<tr style="cursor: pointer;">
 				<td><code>https://url.com</code></td>
+				<td><code>#room:url.com</code></td>
 				<td>Location</td>
 				<td>Owner</td>
 				<td>Info</td>
@@ -39,13 +44,16 @@ export function init(){
 		tr.bind("click", () => {
 			$("#form-connect-homeserver-url")
 				.val(homeserver.url);
+			$("#form-connect-room-alias")
+				.val(homeserver.room);
 			Materialize.updateTextFields();
 		});
 
 		tr.find("td:nth-child(1) code").text(homeserver.url);
-		tr.find("td:nth-child(2)").text(homeserver.location);
-		tr.find("td:nth-child(3)").text(homeserver.owner);
-		tr.find("td:nth-child(4)").text(homeserver.info);
+		tr.find("td:nth-child(2) code").text(homeserver.room);
+		tr.find("td:nth-child(3)").text(homeserver.location);
+		tr.find("td:nth-child(4)").text(homeserver.owner);
+		tr.find("td:nth-child(5)").text(homeserver.info);
 
 		homeserverList.append(tr);
 	}
@@ -53,9 +61,10 @@ export function init(){
 
 	$("#form-connect-homeserver").bind("submit", () => {
 		let url = $("#form-connect-homeserver-url").val();
+		let roomAlias = $("#form-connect-room-alias").val();
 
 		storage.homeserverURL = url;
-		storage.roomAlias = "#informo-test:matrix.org"; // TODO: make a form for this
+		storage.roomAlias = roomAlias;
 		sidebarPage.updateState();
 		matrix.getConnectedMatrixClient()
 			.then((client) => {
